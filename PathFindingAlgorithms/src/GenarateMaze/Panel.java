d3c492bf13332642c25dc1b3405de5dfb53b8298
DucNT17 <95153711+DucNT17@users.noreply.github.com>
2022-11-10 21:03:41 +0700

    Update Panel.java

--- PathFindingAlgorithms/src/GenarateMaze/Panel.java
+++ PathFindingAlgorithms/src/GenarateMaze/Panel.java
@@ -29,13 +29,15 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 	private JLabel speedLabel;
 	private JSlider speedSlider;	
 	private JCheckBox mazeCheckbox;
+	private JLabel algoBoxLabel;
+	private JComboBox algoBox;
+	private int mode;
+	private Button startSolvingButton;
+	private Timer timer;
 
 	public Panel(int windowW,int windowH){
 		this.windowW = windowW;
-		this.windowH = windowH;
-		
-
-		
+		this.windowH = windowH;		
 		this.setPreferredSize(new Dimension(windowW,windowH));
 		this.setBounds(0, 0, windowW, windowH);
 		this.setBackground(new Color(250,250,250));
@@ -50,14 +52,14 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 		this.add(smallPanel);
 		
 		
-		//genarate start button
+		// start button
 		startButton = new Button("Start",(int)(smallPanel.getSize().width/2)-120-30, 30, 120, 50,Color.PINK);
 		smallPanel.add(startButton);
 		startButton.addActionListener(this);
 		
 		
 		
-		//genarate remaze button
+		// remaze button
 		reMazeButton = new Button("Re-Maze",(int)(smallPanel.getSize().width/2)+30, 30, 120, 50,Color.CYAN);
 		smallPanel.add(reMazeButton);
 		reMazeButton.addActionListener(this);
@@ -76,12 +78,10 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 		cellSlider.setSnapToTicks(true);
 		cellSlider.setPaintLabels(true);
 		cellSlider.addChangeListener(this);
-
 		cellLabel = new JLabel();
-		cellLabel.setText("Cell's size adjustment: " + cellSlider.getValue());
+		cellLabel.setText("CELL'S SIZE: " + cellSlider.getValue());
 		cellLabel.setBounds(30, 130, 250, 20);
-		cellLabel.setFont(new Font("",Font.BOLD,18));
-		
+		cellLabel.setFont(new Font("",Font.BOLD,18));		
 		smallPanel.add(cellLabel);
 		smallPanel.add(cellSlider);
 		
@@ -94,21 +94,56 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 		speedSlider.setMajorTickSpacing(1);
 		speedSlider.setSnapToTicks(true);
 		speedSlider.setPaintLabels(true);
-		speedSlider.addChangeListener(this);
-		
+		speedSlider.addChangeListener(this);		
 		speedLabel = new JLabel();
-		speedLabel.setText("Speed: " + speedSlider.getValue());
+		speedLabel.setText("SPEED: " + speedSlider.getValue());
 		speedLabel.setBounds(30, 210, 180, 20);
-		speedLabel.setFont(new Font("",Font.BOLD,18));
-				
+		speedLabel.setFont(new Font("",Font.BOLD,18));		
 		smallPanel.add(speedLabel);
 		smallPanel.add(speedSlider);
 		
+		// CheckBox
+		mazeCheckbox = new JCheckBox();
+		mazeCheckbox.setBounds((int)(smallPanel.getSize().width/2)-120-30, 95, 250, 20);		
+		mazeCheckbox.setText("GENERATE INSTANTLY");
+		mazeCheckbox.setFont(new Font("", Font.BOLD, 15));
+		mazeCheckbox.setFocusable(false);
+		smallPanel.add(mazeCheckbox);
 		
+		//AlgoBox
+		algoBoxLabel = new JLabel("Maze Pathfinding");
+		algoBoxLabel.setBounds(smallPanel.getSize().width/2-155, 370, 220, 30);
+		algoBoxLabel.setFont(new Font("",Font.BOLD,15));
+		smallPanel.add(algoBoxLabel);
 		
+		String[] algoList = {"Breadth First Search(BFS)","Depth First Search(DFS)","Dijkstra","A* Search"};// (0 = BFS , 1 = DFS, 2 = Dijkstra, 3 = A* search)
+		algoBox = new JComboBox(algoList);
+		algoBox.setBounds(smallPanel.getSize().width/2-160, 400, 220, 30);
+		algoBox.setFont(new Font("",Font.BOLD,15));
+		algoBox.setFocusable(false);
+		algoBox.addActionListener(this);
+		mode = algoBox.getSelectedIndex(); // (0 == BFS , 1 = DFS, 2 = Dijkstra, 3 = A* search)
+		smallPanel.add(algoBox);
+		
+		// Start
+		startSolvingButton = new Button("START",smallPanel.getSize().width/2-130-20, 300, 130, 60,new Color(255, 231, 122));
+		smallPanel.add(startSolvingButton);
+		startSolvingButton.addActionListener(this);
+		startSolvingButton.setEnabled(false);
+		
+		//Reset
+		resetButton = new Button("RESET",smallPanel.getSize().width/2+20, 300, 130, 60,new Color(44, 95, 45));
+		smallPanel.add(resetButton);
+		resetButton.addActionListener(this);
+		
+		initMaze();
+		timer.start();
 	}
 	
 	
+	private void initMaze() {
+		maze = new Maze();
+	}
 	
 	@Override
 	public void stateChanged(ChangeEvent e) {
