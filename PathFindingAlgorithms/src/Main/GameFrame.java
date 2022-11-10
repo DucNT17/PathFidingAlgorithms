fa72999b9da5816882b884684f534106a4ea8b41
DucNT17 <95153711+DucNT17@users.noreply.github.com>
2022-11-10 21:38:06 +0700

    Update GameFrame.java

--- PathFindingAlgorithms/src/Main/GameFrame.java
+++ PathFindingAlgorithms/src/Main/GameFrame.java
@@ -7,14 +7,14 @@ import GenarateMaze.Panel;
 
 public class GameFrame extends JFrame{
 	public Panel panel;
-	public final int windowWidthSize = 1000;
-	public final int windowHeightSize = 600;
+	public final int WINDOW_WIDTH_SIZE = 1000;
+	public final int WINDOW_HEIGHT_SIZE = 600;
 	GameFrame(){
 		this.setTitle("Maze Generator and Pathfinding Algorithms");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(windowWidthSize, windowHeightSize);
+		this.setSize(WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
 		this.setResizable(false);
-		panel = new Panel(windowWidthSize,windowHeightSize);
+		panel = new Panel(WINDOW_WIDTH_SIZE,WINDOW_HEIGHT_SIZE);
 		this.add(panel);
 		this.pack();
 		this.setLayout(null);
