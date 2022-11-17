800e11abb4bb9a4f46da57559453409291cd3f75
DucNT17 <95153711+DucNT17@users.noreply.github.com>
2022-11-17 22:07:10 +0700

    Update Panel.java

--- PathFindingAlgorithms/src/GenarateMaze/Panel.java
+++ PathFindingAlgorithms/src/GenarateMaze/Panel.java
@@ -11,13 +11,11 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 	
 	private final int mazeSize = 600;
 	private int cellSize = 40;
-	private int slowestDelay = 100;
+	private int slowestDelay = 110;
 	private int delay = 10;
-	private int windowW,windowH;
 	private int running = -1;
-	private int switchCaseVar = 0;	
 	private Maze maze;
-	private Timer timer;
+	private Timer tm;
 	private JPanel smallPanel;
 	private Button startButton;
 	private Button resetButton;
@@ -27,25 +25,22 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 	private JLabel speedLabel;
 	private JSlider speedSlider;	
 	private JCheckBox mazeCheckbox;
-	private JCheckBox BFSCheckbox;
-	private JComboBox algoBox;
+	private JComboBox<?> algoBox;
 	private int mode ;
 	private JLabel algoBoxLabel;
 	private Button startSolvingButton;
 	private boolean flag = true;
 
-	
 	public Panel(int windowW,int windowH){
-		this.windowW = windowW;
-		this.windowH = windowH;
+		tm = new Timer(delay,this);
 		
-		timer = new Timer(delay, this);
 		this.setPreferredSize(new Dimension(windowW,windowH));
 		this.setBounds(0, 0, windowW, windowH);
 		this.setBackground(new Color(250,250,250));
 		this.setLayout(null);
 		
-		// create table on the right
+		
+		//cai bang ben phai
 		smallPanel = new JPanel();
 		smallPanel.setPreferredSize(new Dimension((windowW-mazeSize),windowH));
 		smallPanel.setBounds(mazeSize, 0, windowW-mazeSize, windowH);
@@ -53,15 +48,15 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 		this.add(smallPanel);
 		
 		
-		// start button
-		startButton = new Button("START",(int)(smallPanel.getSize().width/2)-120-30, 30, 120, 50,Color.PINK);
+		//nut start
+		startButton = new Button("Start",(int)(smallPanel.getSize().width/2)-120-30, 30, 120, 50,Color.PINK);
 		smallPanel.add(startButton);
 		startButton.addActionListener(this);
 		
 		
 		
-		// remaze button
-		reMazeButton = new Button("RE-MAZE",(int)(smallPanel.getSize().width/2)+30, 30, 120, 50,Color.CYAN);
+		//nut re-maze
+		reMazeButton = new Button("Re-Maze",(int)(smallPanel.getSize().width/2)+30, 30, 120, 50,Color.CYAN);
 		smallPanel.add(reMazeButton);
 		reMazeButton.addActionListener(this);
 		reMazeButton.setEnabled(false);
@@ -79,66 +74,74 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 		cellSlider.setSnapToTicks(true);
 		cellSlider.setPaintLabels(true);
 		cellSlider.addChangeListener(this);
+
 		cellLabel = new JLabel();
-		cellLabel.setText("CELL'S SIZE: " + cellSlider.getValue());
+		cellLabel.setText("Cell's size adjustment: " + cellSlider.getValue());
 		cellLabel.setBounds(30, 130, 250, 20);
-		cellLabel.setFont(new Font("",Font.BOLD,18));		
+		cellLabel.setFont(new Font("",Font.BOLD,18));
+		
 		smallPanel.add(cellLabel);
 		smallPanel.add(cellSlider);
 		
 		
-		
-		// Bang lua chon speed
+		//Bang lua chon speed va label cua no
 		speedSlider = new JSlider(1,5,5);
-		speedSlider.setBounds((windowW - mazeSize)/2 - 175, 230, 350, 40); 
+		speedSlider.setBounds((windowW-mazeSize)/2-(350/2), 230, 350, 40);
 		speedSlider.setPaintTrack(true);
 		speedSlider.setMajorTickSpacing(1);
 		speedSlider.setSnapToTicks(true);
 		speedSlider.setPaintLabels(true);
-		speedSlider.addChangeListener(this);		
+		speedSlider.addChangeListener(this);
+
 		speedLabel = new JLabel();
-		speedLabel.setText("SPEED: " + speedSlider.getValue());
+		speedLabel.setText("Speed: " + speedSlider.getValue());
 		speedLabel.setBounds(30, 210, 180, 20);
-		speedLabel.setFont(new Font("",Font.BOLD,18));		
+		speedLabel.setFont(new Font("",Font.BOLD,18));
+				
 		smallPanel.add(speedLabel);
 		smallPanel.add(speedSlider);
 		
-		// CheckBox
+		
+		//generate Check box
 		mazeCheckbox = new JCheckBox();
-		mazeCheckbox.setBounds((int)(smallPanel.getSize().width/2)-120-30, 95, 250, 20);		
-		mazeCheckbox.setText("GENERATE INSTANTLY");
-		mazeCheckbox.setFont(new Font("", Font.BOLD, 15));
-		mazeCheckbox.setFocusable(false);
+		mazeCheckbox.setBounds((int)(smallPanel.getSize().width/2)-120-30, 95, 250, 20);
+		mazeCheckbox.setText("Generate instantly");
+		mazeCheckbox.setFont(new Font("",Font.BOLD,15));
+		mazeCheckbox.setFocusable(false);	
 		smallPanel.add(mazeCheckbox);
 		
-		//AlgoBox
+		
 		algoBoxLabel = new JLabel("Maze Pathfinding");
 		algoBoxLabel.setBounds(smallPanel.getSize().width/2-155, 370, 220, 30);
 		algoBoxLabel.setFont(new Font("",Font.BOLD,15));
 		smallPanel.add(algoBoxLabel);
 		
-		String[] algoList = {"Breadth First Search(BFS)","Depth First Search(DFS)","Dijkstra","A* Search"};// (0 = BFS , 1 = DFS, 2 = Dijkstra, 3 = A* search)
+		
+		
+		String[] algoList = {"Breadth First Search(BFS)","Depth First Search(DFS)","DijkstraSearch","A* Search"};
 		algoBox = new JComboBox(algoList);
 		algoBox.setBounds(smallPanel.getSize().width/2-160, 400, 220, 30);
 		algoBox.setFont(new Font("",Font.BOLD,15));
 		algoBox.setFocusable(false);
 		algoBox.addActionListener(this);
-		mode = algoBox.getSelectedIndex(); // (0 == BFS , 1 = DFS, 2 = Dijkstra, 3 = A* search)
+
+		mode = algoBox.getSelectedIndex(); 
 		smallPanel.add(algoBox);
+
 		
-		// Start
-		startSolvingButton = new Button("START FINDING",smallPanel.getSize().width/2-130-20, 300, 130, 60,new Color(255, 231, 122));
+		//BFS button
+		startSolvingButton = new Button("Start",smallPanel.getSize().width/2-130-20, 300, 130, 60,new Color(255, 231, 122));
 		smallPanel.add(startSolvingButton);
 		startSolvingButton.addActionListener(this);
 		startSolvingButton.setEnabled(false);
 		
-		//Reset
-		resetButton = new Button("RESET",smallPanel.getSize().width/2+20, 300, 130, 60,new Color(44, 95, 45));
+		//nut resetMaze
+		resetButton = new Button("Reset Maze",smallPanel.getSize().width/2+20, 300, 130, 60,new Color(44, 95, 45));
 		smallPanel.add(resetButton);
 		resetButton.addActionListener(this);
-		
+				
 		initMaze();
-		timer.start();
+		tm.start();
 	}
 	
 	
@@ -165,10 +168,9 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 				mazeCheckbox.setEnabled(true);
 				startSolvingButton.setEnabled(true);
 				resetButton.setEnabled(true);
-				timer.stop();
+				tm.stop();
 			}
 		}
-		
 		if(!mazeCheckbox.isSelected()) {
 			maze.mazeAlgorithm(g);
 		}	
@@ -177,80 +179,88 @@ public class Panel extends JPanel implements ActionListener,ChangeListener{
 		}
 	}
 	private void reset() {
-		timer.start();
+		tm.start();
 		running = -1;	
 		startButton.setEnabled(true);
 		repaint();
 	}
-	@Override
-	public void stateChanged(ChangeEvent e) {
-		
-		// cellSlider
-		if(e.getSource()==cellSlider) {
-			if(cellSlider.getValue()%5==0) {
-				mazeCheckbox.setEnabled(true);
-				cellLabel.setText("CELL'S SIZE: " + cellSlider.getValue());
-				cellSize = cellSlider.getValue();
-				startButton.setText("START");
-				flag = true;
-				initMaze();
-				reset();
-			}
-		}
-		
-		// speedSlider
-		if(e.getSource()==speedSlider) {
-			speedLabel.setText("SPEED: " + speedSlider.getValue());
-			delay = slowestDelay - (speedSlider.getValue()-1)* ((slowestDelay-10)/4) ;
-			timer.setDelay(delay);
-			System.out.println(delay);
-	}
-	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		// nut reMaze tao Maze moi
+		//nhan nut re-maze, tao lai maze moi
 		if(e.getSource()==reMazeButton) {
 			flag = true;
 			mazeCheckbox.setEnabled(true);
 			initMaze();
 			reset();
-			startButton.setText("START");
+			startButton.setText("Start");
 		}
-		//  Start Button
+		
+		
+			
+		//nhan nut start 
 		if (e.getSource()==startButton) {
 			running *= -1;
 			if(running == 1)
-				startButton.setText("PAUSE");
+				startButton.setText("Pause");
 			else
-				startButton.setText("START");
+				startButton.setText("Start");
 		}
 		
-		// algoBox
+	
+
 		if(e.getSource()==algoBox) {
 			mode = algoBox.getSelectedIndex();
 		}
-		
-		// bat dau tim duong bang thuat toan
+
+		//start cua ben thuat toan tim duong
 		if(e.getSource() == startSolvingButton) {
 			mazeCheckbox.setSelected(false);
-			mazeCheckbox.setEnabled(false);			
+			mazeCheckbox.setEnabled(false);
+			
 			if(flag) {
 				maze.initStartAndEnd();
 				flag = false;
 			}
 			
-			
-			// reset Maze
-			if(e.getSource()==resetButton) {
-				flag = true;
-				maze.resetMaze();
-				timer.start();
-			}
-			if(running==1)
-				repaint();
 		}
+		
+		
+		//nut reset maze
+		if(e.getSource()==resetButton) {
+			flag = true;
+			maze.resetMaze();
+			tm.start();
+		}
+		if(running==1)
+			repaint();
 	}
+
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
 	
+		//cellSlider
+		if(e.getSource()==cellSlider) {
+			if(cellSlider.getValue()%5==0) {
+				mazeCheckbox.setEnabled(true);
+				cellLabel.setText("Cell's size adjustment: " + cellSlider.getValue());
+				cellSize = cellSlider.getValue();
+				startButton.setText("Start");
+				flag = true;
+				initMaze();
+				reset();
+			}
+		}
+		//SpeedSlider
+		if(e.getSource()==speedSlider) {
+				speedLabel.setText("Speed: " + speedSlider.getValue());
+				delay = slowestDelay - (speedSlider.getValue()-1)* ((slowestDelay-10)/4) ;
+				tm.setDelay(delay);
+				System.out.println(delay);
+		}
+
+	}
 }
+
+	
