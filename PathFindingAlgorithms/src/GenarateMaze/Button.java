aef85577c703f77d3e32c94ab39b425394867add
DucNT17 <95153711+DucNT17@users.noreply.github.com>
2022-11-17 22:05:32 +0700

    Update Button.java

--- PathFindingAlgorithms/src/GenarateMaze/Button.java
+++ PathFindingAlgorithms/src/GenarateMaze/Button.java
@@ -2,13 +2,13 @@ package GenarateMaze;
 
 import java.awt.Color;
 
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class Button extends JButton{
-	Button(String name, int x, int y, int width, int height, Color color){
+	Button(String name,int x,int y,int width,int height,Color color){
 		this.setText(name);
 		this.setBounds(x, y, width, height);
-		this.setLocation(x,y);
+		this.setLocation(x, y);
 		this.setBackground(color);
 		this.setHorizontalTextPosition(JButton.CENTER);
 		this.setVerticalTextPosition(JButton.CENTER);
