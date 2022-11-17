4dd537562b63dcebf7ef9443b1a3ab9c11ba944c
DucNT17 <95153711+DucNT17@users.noreply.github.com>
2022-11-17 22:05:57 +0700

    Update Cell.java

--- PathFindingAlgorithms/src/GenarateMaze/Cell.java
+++ PathFindingAlgorithms/src/GenarateMaze/Cell.java
@@ -8,34 +8,30 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Cell {
-	public int row, r , col, c, cellSize;
-	public boolean[] Walls = {true,true,true,true}; // top right bottom left for cell
+public class Cell{
+	public int row,r,col,c,cellSize;
+	public boolean[] Walls = {true,true,true,true}; //top  right  bottom  left
 	public ArrayList<Cell> next;
 	public Cell parent;
-	public boolean visited = false; // for mazeDrawer
+	public boolean visited = false; //for mazeDrawer
 	private int weight = 1;
 	private int fontWeight;
 	private int strokeSize;
-	public boolean visitedPath = false; // for pathFinding
+	public boolean visitedPath = false; //for pathFinder
 	
-	Cell(int row, int col, int cellSize){
+	
+	Cell(int row,int col,int cellSize){
 		this.row = row;
 		this.col = col;
-		r = row * cellSize;
-		c = col * cellSize;
+		r = row*cellSize;
+		c = col*cellSize;
 		this.cellSize = cellSize;
 		next = new ArrayList<Cell>();
 		parent = null;
-		fontWeight = (int)(10 + 0.3 * (cellSize - 20));
-		strokeSize = (int)(5.0 / 9 * ((cellSize/10) - 1)+1);
+		fontWeight = (int)(10+0.3*(cellSize-20)); //cai nay de khi cellsize to thi font to theo
+		strokeSize = (int)((5.0/9 * ((cellSize/10)-1) )+1); // cai nay de khi cellsize to thi stroke to theo
 	}
-	public void resetCell() {
-		visited = false;
-		visitedPath = false;
-		parent = null;
-		next = new ArrayList<>();
-	}
+	
 	public void drawCell(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		
@@ -53,6 +49,13 @@ public class Cell {
 		
 		g2.setStroke(new BasicStroke(1));
 	}
+
+	public void drawWeight(Graphics g) {
+		g.setFont(new Font("",Font.BOLD,fontWeight));
+		g.drawString(""+weight, (int)(c+cellSize/2)-(int)(fontWeight/3), (int)(r+cellSize/2)+(int)(fontWeight/3));
+		
+	}
+
 	public void drawBox(Graphics g,Color color) {
 		Graphics2D g2 = (Graphics2D) g;
 		if(this.visited == true) {
@@ -60,49 +63,49 @@ public class Cell {
 			g2.fillRect(c, r, cellSize, cellSize);
 		}
 	}
-	public void drawWeight(Graphics g) {
-		g.setFont(new Font("", Font.BOLD, fontWeight));
-		g.drawString("" + weight,(int)(c +  cellSize/2) 
-				- (int)fontWeight/3, (int)(r+cellSize/2) + (int)fontWeight/3);
-	}
-	public void drawPath(Graphics g, Color color) {
-		Graphics2D g2 = (Graphics2D)g;
+	
+	public void drawPath(Graphics g,Color color) {
+		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(color);
-		if(parent != null) {
+		if(parent!=null) {
 			if(this.visitedPath == true) {
-				g2.fillRect(c + (int)(cellSize/3), r + (int) cellSize/3, 
-						(int) cellSize/3, (int)cellSize/3);
+				g2.fillRect(c+(int)(cellSize/3), r+(int)(cellSize/3), (int)(cellSize/3), (int)(cellSize/3));
 			}
-			if(row - 1 == parent.row) { // top of the cell
-				g2.fillRect(c+(int)(cellSize/3), r, 
-						(int)(cellSize/3), (int)(cellSize/3));
-				g2.fillRect(parent.c+(int)(cellSize/3), parent.r+((int)(cellSize/3)*2),
-						(int)(cellSize/3), (int)(cellSize/3));
+			if(row - 1 == parent.row) { //top
+				g2.fillRect(c+(int)(cellSize/3), r, (int)(cellSize/3), (int)(cellSize/3));
+				g2.fillRect(parent.c+(int)(cellSize/3), parent.r+((int)(cellSize/3)*2), (int)(cellSize/3), (int)(cellSize/3));
 			}
-			if(col + 1 == parent.col) { // right of the cell
-				g2.fillRect(c+((int)(cellSize/3)*2), r+(int)(cellSize/3),
-						(int)(cellSize/3), (int)(cellSize/3));
-				g2.fillRect(parent.c, parent.r+(int)(cellSize/3),
-						(int)(cellSize/3), (int)(cellSize/3));
+			
+			if(col + 1 == parent.col) { //right
+				g2.fillRect(c+((int)(cellSize/3)*2), r+(int)(cellSize/3), (int)(cellSize/3), (int)(cellSize/3));
+				g2.fillRect(parent.c, parent.r+(int)(cellSize/3), (int)(cellSize/3), (int)(cellSize/3));
 			}
-			if(row + 1 == parent.row) { // bottom of the cell
-				g2.fillRect(c+(int)(cellSize/3), r+((int)(cellSize/3)*2),
-						(int)(cellSize/3), (int)(cellSize/3));
-				g2.fillRect(parent.c+(int)(cellSize/3), parent.r,
-						(int)(cellSize/3), (int)(cellSize/3));
+			
+			if(row + 1 == parent.row) { //bot
+				g2.fillRect(c+(int)(cellSize/3), r+((int)(cellSize/3)*2), (int)(cellSize/3), (int)(cellSize/3));
+				g2.fillRect(parent.c+(int)(cellSize/3), parent.r, (int)(cellSize/3), (int)(cellSize/3));
 			}
-			if(col - 1 == parent.col) { // left of the cell
-				g2.fillRect(c, r+(int)(cellSize/3), (int)(cellSize/3),
-						(int)(cellSize/3));
-				g2.fillRect(parent.c+((int)(cellSize/3)*2), parent.r+(int)(cellSize/3), 
-						(int)(cellSize/3), (int)(cellSize/3));
+			
+			if(col - 1 == parent.col) { //left
+				g2.fillRect(c, r+(int)(cellSize/3), (int)(cellSize/3), (int)(cellSize/3));
+				g2.fillRect(parent.c+((int)(cellSize/3)*2), parent.r+(int)(cellSize/3), (int)(cellSize/3), (int)(cellSize/3));
 			}
+			
+			
 		}
 	}
+	public void resetCell() {
+		visited = false;
+		visitedPath = false;
+		parent = null;
+		next = new ArrayList<>();
+	}
+
 	public int getWeight() {
 		return weight;
 	}
+
 	public void setWeight() {
-		this.weight = new Random().nextInt(9) + 1;
+		this.weight = new Random().nextInt(9)+1;
 	}
- }
+}
